4749622ec8b1f7760466cbeae241d67dfca67a69
Dustin Stacy <dustin.stacy@outlook.com>
2024-11-18 14:23:17 -0600

    update ignition modules

--- ignition/modules/GroupCampaigns.ts
+++ ignition/modules/GroupCampaigns.ts
@@ -4,7 +4,6 @@ const CampaignsModule = buildModule("CampaignsModule", (m) => {
   const protocolFee = 0;
   const protocol = "0x3ef270a74CaAe5Ca4b740a66497085abBf236655";
 
-  // 1. Update the contract name.
   const campaigns = m.contract("GroupCampaigns", [protocolFee, protocol]);
 
   return { campaigns };
--- ignition/modules/GroupToken.ts
+++ ignition/modules/GroupToken.ts
@@ -1,15 +1,23 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { network } from "hardhat";
 import { curveConfig } from "../../helper-hardhat.config";
+import { resolve } from "path";
+import { readFileSync } from "fs";
 
+// This module is only used for testing. 
+// Deployments for the product must be handled through UI when host creates a new group.
 const GroupTokenModule = buildModule("GroupTokenModule", (m) => {
-    const name = 'Test';
-    const symbol = 'TEST';
-    const bcAddress = "0x2D886074B5D6Aa6C824Dc830e525096c6ebc59d9";
-    const hostAddress = "0x3ef270a74CaAe5Ca4b740a66497085abBf236655";
-    const { initialReserve } = curveConfig[network.name];
+  const chainId = network.config.chainId;
+  const deploymentsFilePath = resolve(__dirname, `../deployments/chain-${chainId}/deployed_addresses.json`);
+  const deployments = JSON.parse(readFileSync(deploymentsFilePath, "utf-8"));
 
-   const groupToken = m.contract("GroupToken", [name, symbol, bcAddress, hostAddress], {value: BigInt(initialReserve)});
+  const name = 'Test';
+  const symbol = 'TEST';
+  const bcAddress = deployments["ProxyModule#ERC1967Proxy"];
+  const hostAddress = "0x3ef270a74CaAe5Ca4b740a66497085abBf236655";
+  const { initialReserve } = curveConfig[network.name];
+
+  const groupToken = m.contract("GroupToken", [name, symbol, bcAddress, hostAddress], { value: BigInt(initialReserve) });
 
   return { groupToken };
 });
