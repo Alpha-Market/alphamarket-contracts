1001abc3708e2859a4acf12ca3232d4d22771f85
Dustin Stacy <dustin.stacy@outlook.com>
2024-11-18 14:24:33 -0600

    update configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,26 +3,46 @@ import "@nomicfoundation/hardhat-toolbox";
 import { vars } from "hardhat/config";
 
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const TESTING_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const ARBISCAN_API_KEY = vars.get("ARBISCAN_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.26",
   etherscan: {
     apiKey: {
       sepolia: ETHERSCAN_API_KEY,
-    }
+      arbitrumSepolia: ARBISCAN_API_KEY,
+    }, 
+    customChains: [
+      {
+        network: "arbitrumSepolia",
+        chainId: 421614,
+        urls: {
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io/",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: true,
   },
   networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+      accounts: [TESTING_PRIVATE_KEY],
     },
     polygonzkEVM: {
       chainId: 2442,
       url: `https://polygonzkevm-cardona.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+      accounts: [TESTING_PRIVATE_KEY],
     },
+    arbitrumSepolia: {
+      chainId: 421614,
+      url: `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [TESTING_PRIVATE_KEY],
+    }
   }, 
 };
 
--- helper-hardhat.config.ts
+++ helper-hardhat.config.ts
@@ -10,6 +10,15 @@ export const curveConfig: networkConfigInfo = {
         reserveRatio: "500000",
         maxGasLimit: "1000000"
     },
+    arbitrumSepolia: {
+        owner: "0x3ef270a74CaAe5Ca4b740a66497085abBf236655",
+        protocolFeeDestination: "0x3ef270a74CaAe5Ca4b740a66497085abBf236655",
+        protocolFeePercent: "500",
+        feeSharePercent: "0",
+        initialReserve: "100000000000000",
+        reserveRatio: "500000",
+        maxGasLimit: "1000000"
+    },
     hardhat: {
         owner: "0x3ef270a74CaAe5Ca4b740a66497085abBf236655",
         protocolFeeDestination: "0x3ef270a74CaAe5Ca4b740a66497085abBf236655",
