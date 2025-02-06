acb11df09402b9a961c372a450e092c9595e2795
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-05 19:50:31 -0600

    add updated scripts

--- scripts/createSchema.ts
+++ scripts/createSchema.ts
@@ -1,35 +1,35 @@
-import { SchemaRegistry} from "@ethereum-attestation-service/eas-sdk";
+import { SchemaRegistry } from '@ethereum-attestation-service/eas-sdk';
 import { ethers } from 'ethers';
-import { vars } from "hardhat/config";
+import { vars } from 'hardhat/config';
 
 // Configuration constants
 // Use the Schema Registry contract address for the network you are using
-const schemaRegistryContractAddress = "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0"; // Sepolia Schema Registry contract address
+const schemaRegistryContractAddress = '0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0'; // Sepolia Schema Registry contract address
 const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
 
 // Probably better served using parameters to pass in the data to increase reusability
 async function registerSchema() {
     try {
         // Initialize provider and signer
-        const provider = new ethers.JsonRpcProvider(vars.get("ALCHEMY_API_URL"));
-        const signer = new ethers.Wallet(vars.get("SEPOLIA_PRIVATE_KEY"), provider);
+        const provider = new ethers.JsonRpcProvider(vars.get('ALCHEMY_API_URL'));
+        const signer = new ethers.Wallet(vars.get('SEPOLIA_PRIVATE_KEY'), provider);
         schemaRegistry.connect(signer);
 
         // Initialize SchemaEncoder with the schema string
-        const schema = "YOUR_SCHEMA"; // e.g., bytes32 contentHash, string urlOfContent
+        const schema = 'YOUR_SCHEMA'; // e.g., bytes32 contentHash, string urlOfContent
         const revocable = true; // A flag allowing an attestation to be revoked. Applies to all attestations of this schema.
 
         const transaction = await schemaRegistry.register({
             schema,
             revocable,
             // You could add a resolver field here for additional functionality
-          });
-          
+        });
+
         // Optional: Wait for transaction to be validated
         await transaction.wait();
-        console.log("New Schema Created", transaction);
+        console.log('New Schema Created', transaction);
     } catch (error) {
-        console.error("An error occurred:", error);
+        console.error('An error occurred:', error);
     }
 }
 
--- scripts/fetchAttestation.ts
+++ scripts/fetchAttestation.ts
@@ -1,26 +1,26 @@
-import { EAS } from "@ethereum-attestation-service/eas-sdk";
-import { ethers } from "ethers"; // install alongside EAS
-import { vars } from "hardhat/config";
+import { EAS } from '@ethereum-attestation-service/eas-sdk';
+import { ethers } from 'ethers'; // install alongside EAS
+import { vars } from 'hardhat/config';
 
 // Use constants for configurations
 // Use the EAS contract address for the network you are using
-const EAS_CONTRACT_ADDRESS = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia EAS contract address
+const EAS_CONTRACT_ADDRESS = '0xC2679fBD37d54388Ce493F1DB75320D236e1815e'; // Sepolia EAS contract address
 
 // Probably better served using parameters to pass in the data to increase reusability
 async function fetchAttestation() {
     // Initialize EAS and provider
     const eas = new EAS(EAS_CONTRACT_ADDRESS);
-    const provider = new ethers.JsonRpcProvider(vars.get("ALCHEMY_RPC_URL"));
+    const provider = new ethers.JsonRpcProvider(vars.get('ALCHEMY_RPC_URL'));
     eas.connect(provider);
 
     // Define UID as a constant if it's not dynamic
-    const UID = "0x62aa93fbae908100345fe96150c6f57ea70946bd8dec4df99bcdb7186f233e39";
+    const UID = '0x62aa93fbae908100345fe96150c6f57ea70946bd8dec4df99bcdb7186f233e39';
 
     try {
         const attestation = await eas.getAttestation(UID); // This function returns an attestation object
         console.log(attestation);
     } catch (error) {
-        console.error("Error fetching attestation:", error);
+        console.error('Error fetching attestation:', error);
     }
 }
 
